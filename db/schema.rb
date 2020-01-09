d326a780cf90f05ecee3263a2b2aa06a9dfaf2ea
figueroar <raul.a.figueroa@hotmail.com>
2020-01-09 15:00:57 -0500

    user refactored in view and db

--- db/schema.rb
+++ db/schema.rb
@@ -33,8 +33,8 @@ ActiveRecord::Schema.define(version: 2020_01_08_051727) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "name"
+    t.string "uid"
     t.string "email"
     t.string "password_digest"
   end
