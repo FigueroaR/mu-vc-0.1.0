dd8ab16e2eba4be9cca09333aacf3455348af45a
figueroar <raul.a.figueroa@hotmail.com>
2020-01-09 17:03:36 -0500

    nested errors on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   #validations
   has_secure_password
-  #validates :name, presence: true
-  #validates :password, presence: true 
-  #validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true 
+  validates :email, presence: true, uniqueness: true
 
   #associations
   has_many :rsvps
